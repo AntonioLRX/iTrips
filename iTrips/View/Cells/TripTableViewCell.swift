0b7d4cbea3c108fc130ff2b8fcc5c7a10ebe6370
Antonio-Xavier <lucas.antonio1232@gmail.com>
2025-11-07 23:44:50 -0300

    Add TripTableViewCell and XIB for trip display
    Introduced TripTableViewCell.swift and its corresponding XIB layout to display trip information in a table view. The XIB includes labels for trip details, pricing, and an image view, providing a custom cell UI for trip listings.

---
+++ iTrips/View/Cells/TripTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TripTableViewCell.swift
+//  iTrips
+//
+//  Created by Antonio Lucas Reginaldo Xavier on 07/11/25.
+//
+
+import UIKit
+
+class TripTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
